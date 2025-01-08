e18c9a47b166c0272ae48bcd121bdbdd005a3786
finn <finnemans12@gmail.com>
2025-01-08 23:08:07 +0100

    navigatie vanaf categorys naar startsession functioneel

--- Categorys.xaml.cs
+++ Categorys.xaml.cs
@@ -7,7 +7,29 @@ public partial class Categorys : ContentPage
 		InitializeComponent();
 	}
 
-	public string category = "blank";
+	public string chosenCategory = "";
 
+    private async void OnTruthClicked(object sender, EventArgs e)
+	{
+		chosenCategory = "Truth";
+		await Navigation.PushAsync(new ReadyCheck());
+	}
 
+    private async void OnDareClicked(object sender, EventArgs e)
+    {
+        chosenCategory = "Dare";
+        await Navigation.PushAsync(new ReadyCheck());
+    }
+
+    private async void OnWouldYouRatherClicked(object sender, EventArgs e)
+    {
+        chosenCategory = "WouldYouRather";
+        await Navigation.PushAsync(new ReadyCheck());
+    }
+
+    private async void OnMixedClicked(object sender, EventArgs e)
+    {
+        chosenCategory = "Mixed";
+        await Navigation.PushAsync(new ReadyCheck());
+    }
 }
